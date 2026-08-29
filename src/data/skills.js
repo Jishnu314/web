bd9fa903452ea2281e3432f7485c75a31b217eac
jishnu <jishnuoffical5@gmail.com>
2026-08-29 17:21:40 +0530

    Update skills.js

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,11 +1,27 @@
 export const SKILLS = [
   {
-    group: "Programming & Tools",
-    items: ["Embedded C/C++", "Python", "MATLAB & Simulink", "Git","Proteus", "LT spice"],
+    group: "Programming",
+    items: ["Embedded C/C++", "Python", "HTML/CSS", "Assembly Language(ASM)"],
   },
-  
+
   {
     group: "Control & Automation",
     items: ["PID Control", "PLC & Ladder Logic", "SCADA", "IoT / MQTT"],
+  },
+  {
+    group: "PLC programming",
+    items: ["Zelio Soft", "Allen Bradley"],
+  },
+  {
+    group: "Other Software & Tools",
+    items: [
+      "Gazebo",
+      "ROS2 Humble",
+      "RViz",
+      "MATLAB & Simulink",
+      "Git",
+      "Proteus",
+      "LT spice",
+    ],
   },
 ];
